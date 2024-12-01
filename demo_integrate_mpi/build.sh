8b0b3d43962dcf2e72eb16d2f288b851a27d91fe
Sean Bryan <sean.bryan@anu.edu.au>
2024-12-02 10:19:18 +1100

    Build with CMake

--- demo_integrate_mpi/build.sh
+++ demo_integrate_mpi/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-module load intel-compiler/2019.5.281 intel-mpi/2019.5.281
-make
+module purge
+# This does work:
+#module load intel-compiler/2019.5.281 intel-mpi/2019.5.281
+# This does not work:
+module load intel-compiler/2019.5.281 openmpi/4.1.4
+rm -rf build
+cmake -S . -B build
+cmake --build build -v
